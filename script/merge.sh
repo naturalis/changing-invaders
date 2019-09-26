d8b00d2d98aeea3b6a900f3d05cf9f42449b437e
Rutger Vos <rutgeraldo@gmail.com>
2019-09-26 19:37:57 +0200

    Update merge.sh

--- script/merge.sh
+++ script/merge.sh
@@ -4,10 +4,10 @@
 #SBATCH --output=ci-merge.out
 
 DIR=/home/rutger.vos/fileserver/projects/B19005-525/Samples/
-SAMPLES=$(ls ${DIR})
-for SAMPLE in ${SAMPLES}; do
-  cd "${DIR}/${SAMPLE}"
+SAMPLES=$(ls $DIR)
+for SAMPLE in $SAMPLES; do
+  cd $DIR/$SAMPLE
     BAMS=$(ls *.bam)
-    samtools merge -r "@RG\tID:NA\tSM:${SAMPLE}\tPL:ILLUMINA\tPI:NA" -l 9 --threads 48 ${SAMPLE}.bam ${BAMS}
+    samtools merge -r "@RG\tID:NA\tSM:${SAMPLE}\tPL:ILLUMINA\tPI:NA" -l 9 --threads 48 ${SAMPLE}.bam $BAMS
   cd -
 done
