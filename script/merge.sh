b6c2e60fe7a20e56454bccd5c578f5102eb3e622
Rutger Vos <rutgeraldo@gmail.com>
2019-09-26 19:31:39 +0200

    Create merge.sh

---
+++ script/merge.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=ci-merge
+#SBATCH --output=ci-merge.out
+
+DIR=~/fileserver/projects/B19005-525/Samples/
+SAMPLES=$(ls $DIR);
+for SAMPLE in $SAMPLES; do
+  cd $DIR
+    BAMS=$(ls *.bam)
+    samtools merge -l 9 --threads 48 $SAMPLE.bam $BAMS
+  cd -
+done
