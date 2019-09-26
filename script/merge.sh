9c679167ae127f2ce317ec20213e4c522a999bc2
Rutger Vos <rutgeraldo@gmail.com>
2019-09-26 19:51:25 +0200

    Update merge.sh

--- script/merge.sh
+++ script/merge.sh
@@ -7,7 +7,7 @@ DIR=/home/rutger.vos/fileserver/projects/B19005-525/Samples/
 SAMPLES=$(ls $DIR)
 for SAMPLE in $SAMPLES; do
   cd $DIR/$SAMPLE
-    BAMS=$(ls *.bam)
-    samtools merge -r "@RG\tID:NA\tSM:${SAMPLE}\tPL:ILLUMINA\tPI:NA" -l 9 --threads 48 ${SAMPLE}.bam $BAMS
+    ls *.bam > bamlist.fofn
+    samtools merge -r "@RG\tID:NA\tSM:${SAMPLE}\tPL:ILLUMINA\tPI:NA" -l 9 --threads 48 -b bamlist.fofn ${SAMPLE}.bam
   cd -
 done
