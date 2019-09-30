ff2dc45975985c14851d7fd27323986ce7466ec8
Rutger Vos <rutgeraldo@gmail.com>
2019-09-30 17:22:05 +0200

    Update merge.sh

--- script/merge.sh
+++ script/merge.sh
@@ -8,7 +8,9 @@ SAMPLES=$(ls $DIR)
 for SAMPLE in $SAMPLES; do
   cd $DIR/$SAMPLE
     export SAMPLE
-    perl -e 'print printf("\@RG\tID:NA\tSM:%s\tPL:ILLUMINA\tPI:NA", $ENV{SAMPLE})' > rg.txt
-    samtools merge -rh rg.txt -l 9 --threads 16 ${SAMPLE}.bam *.bam
+    if [[ ! -e ${SAMBLE}.bam ]]; then
+      perl -e 'print printf("\@RG\tID:NA\tSM:%s\tPL:ILLUMINA\tPI:NA", $ENV{SAMPLE})' > rg.txt
+      samtools merge -rh rg.txt -l 9 --threads 16 ${SAMPLE}.bam *.bam
+    fi
   cd -
 done
