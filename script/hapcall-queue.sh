3291b59b983ca5c7a555d4242f6df655294231ce
leek-dn <leek@getbackinthe.kitchen>
2019-11-05 09:19:32 +0000

    haplotype caller runt met 16 cores

--- script/hapcall-queue.sh
+++ script/hapcall-queue.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # david noteborn
 # haploide calling dmv Queue
-# (en 44 cores op de hpc)
-# roep mij aan met `sbatch hapcall-queue.sh`
-
-#SBATCH --job-name=que-haplo
-#SBATCH --output=hello-queue.out
+# (en 16 cores op de hpc)
+# roep mij aan met `./hapcall-queue.sh [mogelijke sample naam]`
+[ $# -gt 0 ] && sample=$1 || sample=GMI-4_41656
+sbatch <<< '#!/bin/bash
+#SBATCH --job-name=hap-'$sample'
+#SBATCH --output='$sample'.out
 cd gatk*
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/david.noteborn/lib"
-sample=C0910_41662
-java -Djava.io.tmpdir=tmp -jar Queue.jar -jobRunner Drmaa -S haplotypeCaller.scala -R ../REF/Rattus_norvegicus.Rnor_6.0.dna.toplevel.filtered.fa -I ../$sample.sort.bam -O ../$sample:q.g.vcf -nsc 44 -run
+export R_TELEGRAM_BOT_invadersBot="939730741:AAHnRC-oDDSMJ_qjqmsxcrfcfWkJ6uaXm28"
+java -Djava.io.tmpdir=tmp -jar Queue.jar -jobRunner Drmaa -S haplotypeCaller.scala -R ../REF/Rattus_norvegicus.Rnor_6.0.dna.toplevel.filtered.fa -I ../'$sample'.sort.bam -O ../'$sample'.g.vcf -nsc 16 -run
+Rscript telegramhowto.R "Varianten van '$sample' zijn geteld."'
--- script/indexeer.sh
+++ script/indexeer.sh
@@ -1,5 +1,6 @@
-sample=GMI-4_41656
+[ $# -gt 0 ] && sample=$1 || sample=GMI-4_41656
 sbatch <<< '#!/bin/bash
+export R_TELEGRAM_BOT_invadersBot="939730741:AAHnRC-oDDSMJ_qjqmsxcrfcfWkJ6uaXm28"
 #SBATCH --job-name='$sample'
 ## cp ../rutger.vos/fileserver/projects/B19005-525/Samples/'$sample/$sample'.bam .
 samtools index '$sample'.sort.bam
