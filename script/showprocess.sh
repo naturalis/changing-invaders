55ff6d1b63c5ff095a35940685e575ee386b7df3
leek-dn <leek@getbackinthe.kitchen>
2019-11-07 09:15:26 +0000

    laat status zien van threads tijdens haplotypecaller

---
+++ script/showprocess.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+function ProgressBar {
+# Process data
+let _progress=$(((${1}*1000/${2}*1000)/1000))
+let _done=$(((${_progress}*16)/100))
+let _left=$((160-$_done))
+# Build progressbar string lengths
+_fill=$(printf "%${_done}s")
+_empty=$(printf "%${_left}s")
+_progress=$(sed 's/.$/.&/'<<<${_progress})
+printf "[${_fill// /\#}${_empty// /-}] ${_progress}%%\n"
+}
+ls /home/d*n*/gatk*/.queue/scatterGather/haplotypeCaller-1-sg/temp_*_of_16/*.sort.g.vcf.gz.out |xargs -L1 tail -1|sed -E 's/ +/ /g'|cut -d' ' -f11|sed 's/[%\\.]//g'|while read number;do ProgressBar ${number} 1000;done
