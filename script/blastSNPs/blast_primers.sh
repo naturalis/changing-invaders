c2ac0daddfe2bccb05c037ae8e83f11d6df25062
leek-dn <leek@getbackinthe.kitchen>
2019-12-19 14:24:09 +0000

    consensus indexing script toegevoegd, blast script verbeterd

--- script/blastSNPs/blast_primers.sh
+++ script/blastSNPs/blast_primers.sh
@@ -18,12 +18,15 @@ if [ "" != "$fasta" ];then
 #SBATCH --job-name=blast-"'"$fasta"'"
 #SBATCH --output="'"${out%_*}_${db%%_*}.json"'" # output naam = filtered_snps_R7129.json
 date > "'"${out%_*}_${db%%_*}.date"'"
-blastn -num_threads '$threads' -outfmt 13 -max_target_seqs 20 -max_hsps 20 -query "'"$fasta"'" -db "'"$db"'" && \
-Rscript $HOME/telegramhowto.R "'"$fasta"' is geblast! ($(date))"||
-Rscript $HOME/telegramhowto.R "Iets fout gedaan tijdens het blasten van '"$fasta"' sequenties($(date))"
-egrep '"num"|"query_id"' "'"${out%_*}_${db%%_*}.json"'" |egrep -B1 ' 1,|query_id'| sed -En '/--/!s/ {10,14}//p' | awk -F\" 'BEGIN{a="";b=0}/query/{if (a!=""){print a","b;b=0};a=$4}/num/{b++}END{print a","b}'|cut -d_ -f2 |sed -ne N\;s/\\n/,/ -e '/,.*,/p' > blast_output/numlines_"'"${db%%_*}"'".txt
-egrep '^([0-9]+,2,?){2}$' blast_output/numlines_"'"${db%%_*}"'".txt|cut -d, -f1|sed 's/.*/_&"/'|tr \\n \||sed 's/|$//' |egrep -f - "'"${out%_*}_${db%%_*}.json"'" -A1|grep title|cut -d\" -f4|grep -f- filtered_snps_seq.fasta -A1|grep -v '^--$' > "'"${out%_*}_${db%%_*}.json"'"
-
+blastn -num_threads '$threads' -outfmt 13 -max_target_seqs 20 -max_hsps 20 -query "'"$fasta"'" -db "'"$db"'" && {
+	Rscript $HOME/telegramhowto.R "'"$fasta"' is geblast! ($(date))"
+} || {
+	Rscript $HOME/telegramhowto.R "Iets fout gedaan tijdens het blasten van '"$fasta"' sequenties($(date))"
+	exit
+}
+egrep '\''"num"|"query_id"'\'' "'"${out%_*}_${db%%_*}.json"'" |egrep -B1 " 1,|query_id"| sed -En '\''/--/!s/ {10,14}//p'\'' | awk -F\" '\''BEGIN{a="";b=0}/query/{if (a!=""){print a","b;b=0};a=$4}/num/{b++}END{print a","b}'\''|cut -d_ -f2 |sed -ne N\;s/\\n/,/ -e "/,.*,/p" > blast_output/numlines_"'"${db%%_*}"'".txt
+egrep "^([0-9]+,2,?){2}$" blast_output/numlines_"'"${db%%_*}"'".txt|cut -d, -f1|sed "s/.*/_&"/"|tr \\n \||sed "s/|$//" |egrep -f - "'"${out%_*}_${db%%_*}.json"'" -A1|grep title|cut -d\" -f4|grep -f- "'"$fasta"'" -A1|grep -v "^--$" > "'"${out%_*}_${db%%_*}.fasta"'"
+Rscript $HOME/telegramhowto.R "Er zijn $(($(wc -l "'"${out%_*}_${db%%_*}.fasta"'"|cut -d' ' -f1)/4)) SNPs over."
 date >> "'"${out%_*}_${db%%_*}.date"'"'
   else
    echo "2 argument moet een aantal threads zijn, dit lijkt niet op een voor blast interpreteerbaar getal."
---
+++ script/consensus-genoom/index_consensus.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# david
+# changing invaders
+# indexeer consensus
+# is een consensus genoom voor nodig
+[ $# -gt 0 ] && sample=$1 || sample=GMI-4_41656
+if [ -e "$sample".cns.fa ];then
+ makeblastdb -in "$sample".cns.fa -dbtype nucl && \
+ Rscript $HOME/telegramhowto.R "index voor $sample gemaakt" || \
+ Rscript $HOME/telegramhowto.R "tijdens fasta indexeren toch error"
+else
+ echo "$sample.cns.fa" bestaat niet.
+fi
