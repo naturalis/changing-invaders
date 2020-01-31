761e06842529ad0fef31e47005a1d92491e7748e
leek-dn <leek@getbackinthe.kitchen>
2020-01-31 13:29:28 +0000

    scripts to allow running structure analysis

---
+++ script/voorouderPopulatieStructuur/bcf2bed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# david
+# changing invaders
+[ $# -gt 0 ] && sample=$1 || sample=merge3
+# [[ "$sample" =~ .bcf$ ]]&&echo contains extension||sample="$sample.bcf"
+# enkel wanneer het bestaat
+if [ -e "$sample.bcf" -o -e "$sample.vcf" ];then
+sbatch -D $PWD <<< '#!/bin/bash
+#SBATCH --job-name=bcf2bed
+#SBATCH --output=bcf2bed.out
+# --geno toegevoegd
+$HOME/plink $([ -e -e "'"$sample.bcf"'" ]&& echo --bcf "'"$sample"'".bcf||echo --vcf "'"$sample"'".vcf) --memory 5100 --geno --keep-allele-order --vcf-idspace-to _ --const-fid 0 --allow-extra-chr 0 --make-bed --out "'"$sample"'" && {
+ $HOME/telegramhowto.R "[vb]cf bestand is omgezet"
+} || {
+ $HOME/telegramhowto.R "tijdens omzetten error"
+}'
+else
+ echo "$sample.bcf (of $sample.vcf)" bestaat niet.
+fi
---
+++ script/voorouderPopulatieStructuur/combined_filter_qual_depth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# dit script bewerkt het vcf bestand en filtert alle rijen die niet voldoen aan de threshold eruit
+# de uitvoer is het bewerkte bestand
+# door David
+# changing invaders
+# sbatch_do 'bcftools view /data/david.*/merge8.bcf | python3 $HOME/combined_filter_qual_depth.py | bcftools view -Ob > $HOME/merge8.bcf'
+import sys
+
+nregels = 0
+for regel in sys.stdin:
+	# als het een header regel omvat, laat sowieso zien
+	if regel[0:1] == "#":
+		print(regel, end = '')
+	else:
+		regel = regel.split("\t")
+		diepte = int([s for s in regel[7].split(';') if "DP=" in s][0].split('=')[1])
+		if diepte > 16 and 110 > diepte: # is de diepte binnen de threshold
+			if float(regel[5]) > 99: # is de kwaliteit binnen de threshold
+				regel[2] = "SNP" + str(nregels)
+				print("\t".join(regel), end = '')
+				nregels += 1
+
+print(nregels, "geschreven. Wat fijn.", file=sys.stderr)
