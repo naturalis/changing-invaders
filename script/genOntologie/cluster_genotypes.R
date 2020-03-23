e67b58b202649e1b25c8ceadd257827ef7605f72
leek-dn <leek@getbackinthe.kitchen>
2020-03-23 12:58:02 +0000

    beter cluster genotypes script

--- script/genOntologie/cluster_genotypes.R
+++ script/genOntologie/cluster_genotypes.R
@@ -1,19 +1,32 @@
 #!/usr/bin/env Rscript
-# controlleer op sginificante groepen
+# maak MCA voor genotypes
 # changeing invaders
+# by david
 # cut -d, -f7-14 SNP.csv|sed '1s/_[^,]*//g'|tr , \\t|awk '{print $1"\t"$4"\t"$2"\t"$3"\t"$5"\t"$6"\t"$7"\t"$8}'
-setwd("~/Documenten/Naturalis/genen-ontologie/")
+# ggplot voor het plotten van de MCA
 library(ggplot2)
+# ggrepel zodat labels niet op elkaar komen te zitten en het lezen van de samplenamen onmogelijk wordt
 library(ggrepel)
-
-antwoord <- menu(list.files(pattern = "*.gt"), graphics = TRUE, "Kies een genotype bestand")
-if (antwoord != 0) {
-	ASM <- read.table(list.files(pattern = "*.gt")[antwoord], TRUE, stringsAsFactors = TRUE)
+if (length(commandArgs(trailingOnly=T))>0) verwerken <- commandArgs(trailingOnly=T) else {
+	gt_bestanden <- list.files(pattern = "*.gt")
+	# geeft indien mogelijk een grafisch menu met alle .gt bestanden, waaruit de gebruiker een of meerdere voor de MCA kan kiezen
+	verwerken <- select.list(gt_bestanden, multiple = TRUE, title = "Kies een genotype bestand")
+}
+# voor ieder gekozen gt bestand
+for (genotypes in verwerken) {
+	# lees het gt bestand in
+	ASM <- read.table(genotypes, TRUE, stringsAsFactors = TRUE)
+	# voer de MCA uit
 	massa <- MASS::mca(as.data.frame(t(ASM)))
+	# neem de tabel met posities
 	dim.2 <- as.data.frame(massa$fs)
 	dim.2$sample <- rownames(dim.2)
-	ggplot(dim.2, aes(`1`, `2`)) + geom_point() + geom_label_repel(aes(label = sample)) + xlab("MCA dimension 1") + ylab("MCA dimension 2") + ggtitle("Multiple Correspondence Analysis over all Anatomical\nStructure Morphogenesis coding SNPs", subtitle = "Discarding all sysnonymous coding SNPs")
+	# plot de posities
+	ggplot(dim.2, aes(`1`, `2`)) + geom_point() + geom_label_repel(aes(label = sample)) + xlab("MCA dimension 1") + ylab("MCA dimension 2") +
+		ggtitle(paste0("Multiple Correspondence Analysis over alle ", sub(".gt$", "", genotypes), " SNPs"), subtitle = "Discarding all sysnonymous coding SNPs")
 	# awk '$4==$6&&$4!=$1&&$4!=$2&&$4!=$3&&$4!=$5&&$4!=$7&&$4!=$8{print}' ASM.gt
-  ggsave(sub(".gt$", "_msa.png", list.files(pattern = "*.gt")[antwoord]))
-  save(massa, file = sub(".gt$", ".mca", list.files(pattern = "*.gt")[antwoord]))
+	# sla plot op
+	ggsave(sub(".gt$", "_msa.png", genotypes))
+	# sla MCA data op (zodat opnieuw geplot kan worden zonder hele analyse opnieuw te doen)
+	save(massa, file = sub(".gt$", ".mca", genotypes))
 }
