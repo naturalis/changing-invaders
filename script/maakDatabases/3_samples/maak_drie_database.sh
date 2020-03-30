7c53fea03ac0aed2ef929b2898de4d59a8b2ed60
leek-dn <leek@getbackinthe.kitchen>
2020-03-30 16:29:35 +0200

    database scripts opgeschoont en herstructureerd

--- script/maakDatabases/3_samples/maak_drie_database.sh
+++ script/maakDatabases/3_samples/maak_drie_database.sh
@@ -4,7 +4,7 @@
 # changing invaders
 sbatch <<< '#!/bin/bash
 #SBATCH --job-name=maak-DB
-export R_TELEGRAM_BOT_invadersBot="939730741:AAHnRC-oDDSMJ_qjqmsxcrfcfWkJ6uaXm28"
+export R_TELEGRAM_BOT_invadersBot="TOKEN"
 rm onenucleotide_drie.db
 bcftools view merge3.bcf|python3 bewerk_bcf.py | cat maak_drie.sql - | sqlite3 onenucleotide_drie.db
 [ $? -ne 0 ] && Rscript telegramhowto.R "$(ls -t ~/slurm-*.out|head -1|xargs cat)" || Rscript telegramhowto.R "Database met drie individuen is aangemaakt"'
--- script/uitSQLhalen/README.md
+++
@@ -1,2 +0,0 @@
-# scripts voor het maken van vcf dmv de database
-Ik weet niet of "neem terug" *v2* en **v3** hetzelfde functioneren.
