e5dcb9ba93f28b18dfa4cb17e6cd2f689d89f6d5
leek-dn <leek@getbackinthe.kitchen>
2020-04-16 15:47:44 +0200

    translated comments to English uniek-meer.R, improved README and added flowchart

--- script/blastSNPs/README.md
+++ script/blastSNPs/README.md
@@ -1,12 +1,5 @@
-# BLASTing
+# BLAST SNP flanking regions
 
-De SNPs locaties zijn opgeslagen als filtered_snps.csv. Door middel van dna_van_ref.R zal een csv bestand gemaakt worden met de positites, en de 250 basen voor en na de SNP (en daarnaast de SNP ref allele zelf)
-Door middel van de sed expressie
-`sed -nE '1!{s/([0-9]+,[0-9]+),"([^"]+)","([^"]+)","(.)"/>\1-\4\n\2\n>\1-\4\n\3/p}' filtered_snps_seq.csv > filtered_snps_seq.fasta`
-wordt een fasta bestand aangemaakt met de voor/na sequentie(s) als losse records.
-Dit fasta bestand kan worden gebruikt door blast_primers.sh om de sequenties te zoeken in een genoom.
-Dit kan door het script `blast_primers.sh`.
-De output is een bestand dat bijna json is. (afgezien van een datum aan het einde)
-Het zoekt 20 hits (met 20 HSP). Het is bekend dat dit niet per ce de beste 20 hits zijn (door paper), maar dat is voor het gebruik van het zoeken voor primers niet belangrijk.
-Ook hoort bij de output een fasta bestand `filtered_snps_(korte sample naam).fasta` met alle sequenties die exact 1 keer voor kwamen.
-Dit bestand kan vervolgens weer gebruikt worden om opnieuw te BLASTen met een ander consensus genoom.
+To know whether a SNP would be a good candidate for KASP, the flanking regions of the SNP are BLASTed against consensus genomes. First the filtered SNP flanking regions should be extracted as fasta (uniek-meer.R). Then BLASTing is done. This is done by two scripts. However because one can BLAST against all consensus genomes at once, or just one, two scripts are made: blast_primers.sh (for one consensus genome) and blast_all_primers.sh (for BLASTing all consensus genomes). blast_output.R is the second script that checks whether an alignment actually contains a INDEL (insetion/deletion), since primers will not align with gaps. (As one can see this is a bit too conservative and actually might have a way bigger gap cost to resolve this problem).
+See also the flowchart:
+![flowchart](flowchart.png?raw=true)
--- script/blastSNPs/uniek-meer.R
+++ script/blastSNPs/uniek-meer.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
-#SBATCH --job-name=filter-SNP
-# filter SNPs
+#SBATCH --job-name=unique-SNP
+# changing invaders
+# unique more
 # by david
 # biostrings is nodig (BiocManager::install("Biostrings"))
 library(Biostrings)
@@ -11,8 +12,8 @@ if (!is.na(commandArgs(trailingOnly=TRUE)[1])) db = commandArgs(trailingOnly=TRU
 Sys.time()
 eightnucleotide <- dbConnect(SQLite(), db)
 exulans <- tbl(eightnucleotide, "EXULANS")
-# groepeer op SNP, maak een variable die aangeeft of het een heterozygote SNP is, en of de coverage, quality en afstand voldoet
-# filter op die variabelen
+# group on SNP, make a variable that explains whether it is a heterozygote SNP, and whether the coverage, quality and distance comply
+# filter on these variables
 zoekterm <- exulans %>% group_by(chromosome, position) %>% summarise(heterozygoot = !(n_distinct(paste0(GENOTYPE_BP)) == 1L && count(GENOTYPE_BP) == 8L), p = COUNT(REFERENCE), COVERAGE_THRESHOLD = mean(COVERAGE, na.rm = TRUE) > 16L && 110L > mean(COVERAGE, na.rm = TRUE), QUALITY_THRESHOLD = mean(QUALITY, na.rm = TRUE) > 99L, DIST_P = min(ifelse(DIST_N==-1, 250, DIST_P), na.rm = TRUE), DIST_N = min(ifelse(DIST_N==-1, 250, DIST_N), na.rm = TRUE)) %>% filter(heterozygoot, COVERAGE_THRESHOLD, QUALITY_THRESHOLD, DIST_N > 249L, DIST_P > 249L)
 # zoekterm <- zoekterm %>% ungroup() %>% summarise(hoeveel = count())
 zoekterm <- zoekterm %>% select(chromosome, position)
@@ -20,43 +21,44 @@ zoekterm %>% show_query()
 gefiltert <- zoekterm %>% collect()
 dbDisconnect(eightnucleotide)
 backup <- gefiltert
-# lees het referentie fasta basetand in.
-# dit duurt wel even
+# read the reference fasta file in.
+# this takes some time
 s = readDNAStringSet(paste0(Sys.getenv("HOME"), "/REF/Rattus_norvegicus.Rnor_6.0.dna.toplevel.filtered.fa"))
-# bewerk de namen zodat het enkel het chromosoom nummer is
+# edit the names so it is only the chromosome number
 names(s) <- mapply(`[`, strsplit(names(s), " "), 1)
 # gefiltert <- read.table("vanHIGHimpact.pos", col.names = c("chromosome", "position"))
 gefiltert <- gefiltert[gefiltert$chromosome!=0,]
 sequences <- apply(gefiltert, 1, function(x) {
-  # controlleer of flanking regios wel op het chromosoom vallen om errors te voorkomen
+  # check whether flanking regions are on the chromosome to stop errors
   if (length(s[[x["chromosome"]]])>as.numeric(x["position"])+250&as.numeric(x["position"])-250>0)
-    # neem als dat klopt 250 voor tot 250 na de SNP
+    # if that applies, get 250 before, to 250 after the SNP
     toString(subseq(s[[x["chromosome"]]], start=as.numeric(x["position"])-250, end = as.numeric(x["position"])+250)) else NA
 })
-# voeg sequenties toe aan positie tabel
+# add sequences to the position table
 gefiltert$voor <- substr(sequences, 1, 250)
 gefiltert$na <- substr(sequences, 252, 501)
 gefiltert$ref <- substr(sequences, 251, 251)
 gefiltert <- gefiltert[!is.na(sequences),]
-# sla op
+# store it
 setwd("/data/david.noteborn/blast_output")
 write.csv(gefiltert, "filtered_snps.csv", quote = FALSE, row.names = FALSE)
-# maak er een fasta van
-# het csv bestand ziet er meestal zo uit:
-# CHR,POS,seq_voor,seq_na,SNP_base
-# de eerste regel is een header, die wordt overgeslagen (1! in combinatie met -n, wat aangeeft dat niet alles standaard naar de terminal wordt geschreven)
-# vervolgens wordt dit omgezet naar het formaat
+# make it a fasta
+# the csv file looks like this:
+# CHR,POS,seq_before,seq_after,SNP_base
+# the first line is a header, that is skipped (1! in combination with -n, which explains that not everything is written by default to stdout)
+# next it is converted to this format:
 # >CHR,POS-base
-# seq_voor
+# seq_before
 # >CHR,POS-base
-# seq_na
-# dit wordt gedaan door groepen regex te vangen en te vervangen
-# de p aan het einde geeft aan dat dit wel wordt geschreven
-# -E geeft aan dat extended reguliere expressie word gebruikt wat ons het voordeel biedt niet
-# alle groepen als \(inhoud\) maar als (inhoud) te hoeven schrijven en + te gebruiken (1 of meer ipv * 0 of meer)
+# seq_after
+# this is done by groupregex catch and replace
+# the p in the end explains that this should be written to stdout
+# -E explains that this is about an extended regular expression which gives us the advantage to not write
+# all groups like \(content\) but like (content) and use + (1 or more instead of * 0 or more)
 system("sed -nE '1!{s/([0-9]+,[0-9]+),([^,]+),([^,]+),(.)/>\\1-\\4\\n\\2\\n>\\1-\\4\\n\\3/p}' filtered_snps.csv > filtered_snps.fasta")
 Sys.time()
+# append here the telegram bot token one wants to use during this analysis
 bot <- TGBot$new(token = "TOKEN")
-bericht <- paste("Er zijn", nrow(gefiltert), "SNPs over tijdens het verbeterde algoritme.")
+bericht <- paste("There are", nrow(gefiltert), "SNPs left during the improved algorithm.")
 bot$sendMessage(bericht, chat_id = 0)
 cat(bericht)
