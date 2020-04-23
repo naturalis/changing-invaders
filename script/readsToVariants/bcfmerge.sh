dc6fca5e86684498754ea84181fcade39cc588b6
leek-dn <leek@getbackinthe.kitchen>
2020-04-23 14:13:14 +0200

    forgot to merge a file

--- script/readsToVariants/bcfmerge.sh
+++ script/readsToVariants/bcfmerge.sh
@@ -9,12 +9,12 @@
 # get from all the samples the sample whose name contains merge (or first argument) extract the number
 # get the number of samples - those that have merge in their name
 # look to both as a big sum and summarize
-getal=$(($(<<<"$samples" grep merge|sed -E s/.*merge\(\[0-9\]+\).bcf/\\1/|tr \\n +|sed s/+$//)+$(<<<"$samples" grep -vc merge)))
+number=$(($(<<<"$samples" grep merge|sed -E s/.*merge\(\[0-9\]+\).bcf/\\1/|tr \\n +|sed s/+$//)+$(<<<"$samples" grep -vc merge)))
 sbatch -D $PWD -n2<<< '#!/bin/bash
-#SBATCH --job-name=merge" "'$getal'
- bcftools merge --threads 2 -0 -m snps -Ob '$samples' -o merge'$getal'.bcf && {
+#SBATCH --job-name=merge" "'$number'
+ bcftools merge --threads 2 -0 -m snps -Ob '$samples' -o merge'$number'.bcf && {
  # seperate samplenames on _ and get the first part, replace newlines by , (comma space) (and the last one by nothing) and the last one by "and"
  $HOME/telegramhowto.R "'$(cut -d_ -f1<<<"$samples"|tr \\n ,|sed -e 's/,$//' -e 's/,/& /g'|rev|sed 's/,/dna /'|rev)' are combined to one"
 } || {
- $HOME/telegramhowto.R "Combining to merge'$getal'.bcf failed"
+ $HOME/telegramhowto.R "Combining to merge'$number'.bcf failed"
 }'
