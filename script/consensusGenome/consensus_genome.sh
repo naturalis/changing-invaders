adcce55c15ce957da30018744449f3dd32601c6d
leek-dn <leek@getbackinthe.kitchen>
2020-04-24 12:19:02 +0200

    improved consensus genome

--- script/consensusGenome/consensus_genome.sh
+++ script/consensusGenome/consensus_genome.sh
@@ -9,11 +9,11 @@ if [ "" != "$bam" ];then
 sbatch -D $PWD<<< '#!/bin/bash
 #SBATCH --job-name=con"'"$sample"'"
 trap '\''$HOME/telegramhowto.R "Something goes wrong during creation of '"$sample"' consensus sequence at line $LINENO (commando: $(sed -n $LINENO"p" "$BASH_SOURCE"))";exit 2'\'' ERR
-bcftools mpileup -f $HOME/REF/Rattus_norvegicus.Rnor_6.0.dna.toplevel.filtered.fa "'"$bam"'" | bcftools call -mv -Oz  -o "'"$sample"'".calls.vcf.gz || error
-$HOME/tabix "'"$sample"'".calls.vcf.gz || error
+bcftools mpileup -f $HOME/REF/Rattus_norvegicus.Rnor_6.0.dna.toplevel.filtered.fa "'"$bam"'" | bcftools call -mv -Oz  -o "'"$sample"'".calls.vcf.gz
+$HOME/tabix "'"$sample"'".calls.vcf.gz
 bcftools consensus "'"$sample"'".calls.vcf.gz -f $HOME/REF/Rattus_norvegicus.Rnor_6.0.dna.toplevel.filtered.fa > "'"$sample"'".cns.fa && {
- $HOME/telegramhowto.R "Out of '"$sample"' a consensus sequence is made";true
-} || error'
+ $HOME/telegramhowto.R "Out of '"$sample"' a consensus sequence is made"
+}'
 else
  echo "$sample*".bam does not exist.
 fi
--- script/consensusGenome/consensus_index.sh
+++ script/consensusGenome/consensus_index.sh
@@ -6,7 +6,7 @@
 [ $# -gt 0 ] && sample=$1 || sample=GMI-4_41656
 if [ -e "$sample".cns.fa ];then
  makeblastdb -in "$sample".cns.fa -dbtype nucl && {
-  $HOME/telegramhowto.R "index for $sample is made"
+  $HOME/telegramhowto.R "index for $sample is made";true
  } || {
   $HOME/telegramhowto.R "during fasta indexing still an error"
  }
