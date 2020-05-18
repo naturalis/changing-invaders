9c52dd0975787dea86f9743c2d364d0483de6dd7
leek-dn <leek@getbackinthe.kitchen>
2020-05-18 16:56:06 +0200

    the actual fix for blast_output.R

--- script/blastSNPs/blast_output.R
+++ script/blastSNPs/blast_output.R
@@ -10,13 +10,16 @@ json_combination <- Map(function(x) fromJSON(x, simplifyVector = FALSE)$BlastOut
 names(json_combination) <- paste(unlist(Map(function(x)x$report$results$search$query_title, json_combination, USE.NAMES = FALSE)), c("before", "after"))
 how_many_hits <- unlist(Map(function(x)length(x$report$results$search$hits)==1&&length(x$report$results$search$hits[[1]]$hsps)==1, json_combination, USE.NAMES = FALSE))
 # table(how_many_hits)
-json_combination <- remove_this(json_combination, !how_many_hits)
+# if they are all true, then nothing should be removed anyway,
+# however due the function it would remove everything
+if(!all(how_many_hits)) json_combination <- remove_this(json_combination, !how_many_hits)
 contains_gap <- mapply(function(x) {
   hit <- x$report$results$search$hits[[1]]$hsps[[1]]
   grepl("-", hit$qseq)|grepl("-", hit$hseq)
 }, json_combination)
 # table(contains_gap)
-json_combination <- remove_this(json_combination, contains_gap)
+# same applies here
+if(!all(!contains_gap)) json_combination <- remove_this(json_combination, contains_gap)
 pos_pairs <- sub(" .*", "", names(json_combination))
 pos_pairs <- pos_pairs[!duplicated(pos_pairs)]
 dna <- readDNAStringSet(paste0(sample, ".fasta"))
