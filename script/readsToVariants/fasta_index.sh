e5917b0733f7fa7eb3fe6fd996d1c3027b76446e
leek-dn <leek@getbackinthe.kitchen>
2020-05-25 16:01:37 +0200

    added file to index reference genome for bcf calling

---
+++ script/readsToVariants/fasta_index.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# changing invaders
+# by David
+# index a fasta file
+[ $# -gt 0 ] && fasta=$1 || fasta=GMI-4_41656
+if [ "$fasta*.fa{,sta}" != "$(echo "$fasta"*.fa{,sta})" ];then
+sbatch -D $PWD<<< '#!/bin/bash
+#SBATCH --job-name=index:"'"$fasta"'"
+samtools faidx "'"$fasta"'"*.fa{,sta}
+$HOME/telegramhowto.R "'"$fasta"' is indexed"'
+else
+ echo "$fasta*".fa{,sta} does not exist "(in $PWD)".
+fi
+
